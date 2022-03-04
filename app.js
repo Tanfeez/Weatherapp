ea505f3ab645a90c3224f7ccd0184ec1908d6af0
Tanfeez <47417098+Tanfeez@users.noreply.github.com>
2022-03-05 01:44:29 +0530

    added weather icon

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var input = document.querySelector('.input_text');
 var main = document.querySelector('#name');
 var temp = document.querySelector('.temp');
 var desc = document.querySelector('.desc');
+var icon = document.querySelector('.icon');
 var button= document.querySelector('.submit');
 const targetDiv=document.getElementById("container");
 targetDiv.style.display = "none";
@@ -21,10 +22,13 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=m
   var tempValue = data['main']['temp'];
   var nameValue = data['name'];
   var descValue = data['weather'][0]['description'];
+  var iconimage = data['weather'][0]['icon'];
+  console.log(iconimage);
 
   main.innerHTML = nameValue;
-  desc.innerHTML = "Description - "+descValue;
-  temp.innerHTML = "Temperature - "+tempValue+"&deg;C";
+  desc.innerHTML = "Description : "+descValue;
+  temp.innerHTML = "Temperature : "+tempValue+"&deg;C";
+  icon.src= 'https://openweathermap.org/img/wn/'+iconimage+'.png';
   input.value ="";
 
 })
